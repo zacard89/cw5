d7fc20b7bbb1c5e861e230183b2be44a659e8d0c
zacard <zacard89@gmail.com>
2020-03-31 23:55:18 +0200

    zadanie 7

--- WebApplication/Controllers/StudentsController.cs
+++ WebApplication/Controllers/StudentsController.cs
@@ -20,5 +20,18 @@ namespace WebApplication.Controllers
             student.IndexNumer = $"s{new Random().Next(1, 20000)}";
             return Ok(student);
         }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id)
+        {
+            return Ok($"Student o id:{id} zostal usunięty");
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult AddStudentPhoto(int id)
+        {
+            return Ok($"Aktualizacja zdjęcia dla studenta o id={id} została zakończona");
+        }
+        
     }
 }
