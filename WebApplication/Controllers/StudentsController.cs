a997db6ab90aa7455319b2cb6b24d00e69b8c067
zacard <zacard89@gmail.com>
2020-03-31 23:17:59 +0200

    zadanie 3

---
+++ WebApplication/Controllers/StudentsController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetStudents()
+        {
+            return "Kowalski,Malewski,Andrzejewski";
+        }
+    }
+}
